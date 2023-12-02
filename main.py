44d70e3d78f026a6680393e3aa553a329a2eb16c
jasur <251tojiboyev@gmail.com>
2023-12-02 10:11:36 +0500

    uy_ishi3

---
+++ main.py
@@ -0,0 +1,14 @@
+Target = int(input("son kirit:"))
+Num = [2, 3, 4, 5, 6, 6, 7, 7, 7]
+
+def find_occurrences(Num, Target):
+    occurrence_count = 0
+
+    for num in Num:
+        if num == Target:
+            occurrence_count += 1
+
+    return occurrence_count
+
+
+print(find_occurrences(Num, Target))
